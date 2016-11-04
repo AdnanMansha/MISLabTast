ad3b64c9ccdbef5bda4a2d92f2e0d5342200fdc5
Muhammad Noman <nomanmuhammad510@ymail.com>
2016-11-05 03:26:03 +0500

    Optimal
    Optimal code file

--- Multiplication.java
+++ Multiplication.java
@@ -1,48 +1,27 @@
 import java.util.Scanner;
 
 public class Multiplication {
-
-	public static void main(String[] args) {
+	public int multiplication (){
+		
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter First argument value: ");
+				Scanner sc = new Scanner(System.in);
+				System.out.print("Enter First argument value: ");
 
-		int a = sc.nextInt();
-		System.out.print("Enter Second argument value: ");
+				int a = sc.nextInt();
+				System.out.print("Enter Second argument value: ");
 
-		int b = sc.nextInt();
-		System.out.print("Multiplication is: ");
-		int c=a*b;
-		System.out.print(c);
+				int b = sc.nextInt();
+				System.out.print("Multiplication is: ");
+				int c=a*b;
+				System.out.println(a+" * "+b+" = "+c);
 
 
-		int d = sc.nextInt();
-		System.out.print("Enter Second argument value: ");
-
-		int e = sc.nextInt();
-		System.out.print("Multiplication is: ");
-		int f=d*e;
-		System.out.print(f);
-		
-		/*
-		 * 
-		 * 
-		 * As discussed in last lab. You are required to create a Git Repository and a small application.
-			Every group member will create his/her own branch and create a new function.
-			 At the end the branhces should be merged into master. 
-			 Afterwards, every group member will pull master in their respective branches for the new code.
-			  Now all members will make changes in the same function at their respective branches. 
-			  Now when the branches get merged into master it would run into conflict mode. 
-			  You need to resolve this conflict and merge all the branches in master. 
-			  Finally, your code will be available at the Production branch.
-			
-			Make sure to give proper comments while commiting from stash to repository.
-			
-			Those who didn't attend the lab have no right to come and ask questions about the basics of Git. 
-			You can get help from the students who were present on 13th and 14th October respectively.
-
-		 * */
-		
+				
+				return c;
+	}
+	public static void main(String[] args) {
+		Multiplication obj=new Multiplication();
+		obj.multiplication();
 	}
 
 }
