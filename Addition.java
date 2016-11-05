827c44963ffdcb934e5df44e177e5168ab100efe
AdnanMansha <rajaadnan.mansha@yopmail.com>
2016-11-05 14:32:22 +0500

    Addition

---
+++ Addition.java
@@ -0,0 +1,26 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Addition 
+{
+	public static void addition()
+	{
+		Scanner in=new Scanner(System.in);
+		System.out.println("\n\nEnter First Number : ");
+		int x=in.nextInt();
+		System.out.println("\n\nEnter Second Number");
+		int y=in.nextInt();
+		System.out.println("\nAns: "+(x+y));
+		in.close();
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("\n\t\tCalcualtor");
+		addition();
+		
+	}
+
+}
